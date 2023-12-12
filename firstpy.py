d6ddc4b123c8580cdc7f599617c04d05e9562dc4
NarawitPrathansap <narawit.p@kkumail.com>
2023-12-12 10:21:23 +0700

    add def flow

--- firstpy.py
+++ firstpy.py
@@ -1,2 +1,10 @@
+def printHello():
+    print('Hello world')
+
+def multiplyby9(inputV):
+    print(9*inputV)
+
 if __name__=="main":
-   print('we are in your area')
+   print('Blackpink in your area')
+   multiplyby9(20)
+   printHello()
