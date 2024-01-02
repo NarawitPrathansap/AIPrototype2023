d72ba4ae23ef61e13fd8151f2d2683c95c3401e7
NarawitPrathansap <narawit.p@kkumail.com>
2024-01-02 09:35:48 +0700

    HW sum

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,21 +2,26 @@ from re import sub
 import subprocess #สำหรับรัน terminal command
 
 if __name__ == "__main__":
+    sum_output = 0
     # basic terminal command
     #subprocess.run(["ls","-ltr"])
     #subprocess.run(["rm","-r","/home/tohn/testfolder1"])
 
     #python commands
     print(f"first run num=100 XX=90")
-    subprocess.run(["python","firstpy.py", "--num", "100", "--XX", "90"])
+    result = subprocess.run(["python","firstpy.py", "--num", "100", "--XX", "90"])
+    sum_output += int(result.stdout.decode('utf-8').split()[0])
     print(f"------------------------------------------------")
     print(f"second run num=-10 XX=-90")
-    subprocess.run(["python","firstpy.py", "--num", "-10", "--XX", "-90"])
+    result = subprocess.run(["python","firstpy.py", "--num", "-10", "--XX", "-90"])
+    sum_output += int(result.stdout.decode('utf-8').split()[0])
     print(f"------------------------------------------------")
     print(f"third run num=0")
-    subprocess.run(["python","firstpy.py", "--num", "0"])
+    result = subprocess.run(["python","firstpy.py", "--num", "0"])
+    sum_output += int(result.stdout.decode('utf-8').split()[0])
     print(f"------------------------------------------------")
 
+    print(f'Sum of the numbers before Hello world!: {sum_output}')
 
 
     #use output from other program
