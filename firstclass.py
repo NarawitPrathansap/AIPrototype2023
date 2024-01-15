0bea492c7903f8ff8ce84a7a2b4ab9c4a2e656d4
NarawitPrathansap <narawit.p@kkumail.com>
2024-01-15 16:12:53 +0700

    home

--- firstclass.py
+++ firstclass.py
@@ -12,5 +12,10 @@ def helloworld():
 def hellowfirst():
     return "Hello, First!"
 
+@app.route("/home2")  
+def home2():
+    return render_template("home.html",name='first')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0',port=5001
