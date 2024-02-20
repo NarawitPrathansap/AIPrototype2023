789438f63fedfec864f7b90c97bf5fa42ff75343
NarawitPrathansap <narawit.p@kkumail.com>
2024-02-20 14:13:38 +0700

    add

--- webapp2.py
+++ webapp2.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
+# Ensure there is a folder named 'uploads' in the same directory as this script
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
-def home():
-    return render_template('web.html')
+def index():
+    return render_template('upload.html')
 
-
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'Image uploaded successfully'
+    
 
 
 if __name__ == "__main__":
