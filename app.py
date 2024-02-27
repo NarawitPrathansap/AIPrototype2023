ded57443570af66f545aff14a35c7f6889f0b76b
NarawitPrathansap <narawit.p@kkumail.com>
2024-02-27 10:58:27 +0700

    add

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for,send_from_directory
 from werkzeug.utils import secure_filename
+from PIL import Image
 import os
 
 app = Flask(__name__)
@@ -35,13 +36,29 @@ def predict():
             filename = secure_filename(image.filename)
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             image.save(image_path)
-            image_url = url_for('uploaded_file', filename=filename)
-            # Print received question for debugging
+            
+            # Load the image
+            img = Image.open(image_path)
+            width, height = img.size
+            
+            # Cut the image in half and flip the left side
+            left_side = img.crop((0, 0, width // 2, height))
+            flipped_left_side = left_side.transpose(Image.FLIP_LEFT_RIGHT)
+            
+            # Create a new image and paste the flipped left side to the right side
+            new_image = Image.new('RGB', (width, height))
+            new_image.paste(flipped_left_side, (width // 2, 0))
+            
+            # Save the modified image
+            new_filename = 'modified_' + filename
+            new_image_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
+            new_image.save(new_image_path)
+            
+            image_url = url_for('uploaded_file', filename=new_filename)
             print("Received question:", question)
             prediction = "Dummy prediction result"  # Replace with your model's prediction logic
             return render_template('result.html', image_url=image_url, question=question, prediction=prediction)
         else:
-            # If the file is not allowed, you can redirect to the upload page with a message
             print('File type not allowed')
             return redirect(request.url)
     return redirect(url_for('index'))
