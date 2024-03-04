8d05a6fdd75b52217cb93d4388563f1b7c1a4907
NarawitPrathansap <narawit.p@kkumail.com>
2024-03-04 12:15:32 +0700

    add

--- model.py
+++ model.py
@@ -1,25 +1,26 @@
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import img_to_array
 from tensorflow.keras.models import load_model
 
-# Correct the import paths according to your environment setup
-# If efficientnet is a custom module, ensure it's correctly installed or available in your project
-from efficientnet.layers import Swish, DropConnect
-from efficientnet.model import ConvKernelInitializer
+import sys
+sys.path.append('/content/gdrive/MyDrive/tooth_information/26_Multi_1e-6_250_Unfreeze.h5')
 
-# Register custom objects with TensorFlow
-custom_objects = {
-    'ConvKernelInitializer': ConvKernelInitializer,
+from efficientnet.layers import Swish, DropConnect
+from efficientnet.model import ConvKernalInitializer
+from tensorflow.keras.utils import get_custom_objects
+
+get_custom_objects().update({
+    'ConvKernalInitializer': ConvKernalInitializer,
     'Swish': Swish,
-    'DropConnect': DropConnect,
-}
+    'DropConnect':DropConnect
+})
+
 
 # Adjust this path to where your model is actually stored
-model_path = "/content/gdrive/MyDrive/Tooth_Shap_GPT/Deep_tooth/Model/Unflipped_Regress_Age(7-23)/Duo/26_Multi_1e-6_250_Unfreeze.h5"
-model = load_model(model_path, custom_objects=custom_objects)
-
+model_path = "/content/gdrive/MyDrive/tooth_information/26_Multi_1e-6_250_Unfreeze.h5"
+model = load_model(model_path, custom_objects=get_custom_objects)
 
 # Preparing and pre-processing the image
 def preprocess_img(img_path):
@@ -37,7 +38,7 @@ def predict_result(img_array):
 
     # Assuming your model returns age as a continuous value and gender as a probability that needs argmax
     # Adjust these lines according to your model's actual output format
-    age = prediction_age[0][0]  # Assuming the first prediction is age as a continuous value
+    age = prediction_age[0] # Assuming the first prediction is age as a continuous value
     gender = np.argmax(prediction_gender[0], axis=-1)  # Assuming the second prediction is gender as a binary classification
 
     return age, gender
